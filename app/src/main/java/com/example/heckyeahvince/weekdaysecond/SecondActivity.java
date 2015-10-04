894affde263d60167a8ac2e20ca4c4ff34eba63c
Paul Vince Abe <peeveeabe10@gmail.com>
2015-10-05 06:13:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/heckyeahvince/weekdaysecond/SecondActivity.java
@@ -0,0 +1,48 @@
+package com.example.heckyeahvince.weekdaysecond;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        handleIntent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void handleIntent(){
+        Intent i = getIntent();
+        String textOut = i.getStringExtra("Weekday");
+        TextView tvOut = (TextView) findViewById(R.id.displayDay);
+        tvOut.setText(textOut);
+    }
+}
